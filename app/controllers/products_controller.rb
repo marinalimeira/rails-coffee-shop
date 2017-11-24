3974695b9c90f83f5e6eb060041ddbce2b886b1b
Marina Limeira <marinaflessa@gmail.com>
2017-11-23 23:39:24 -0200

    Add form and list of products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+     params.require(:product).permit(:weight, :roast, :ground, :price, :quantity)
+  end
 end
